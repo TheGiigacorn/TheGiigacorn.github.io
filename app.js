0062710b989c79c42ab42bb9542fdf353613acd5
TheGiigacorn <brenlieb1@gmail.com>
2021-07-29 18:09:45 -0500

    added scrolling functionality to buttons.
    Buttons with the added functions will now scroll to a specified location on the webpage. Some buttons are not in place because other elements are not correct.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 'use strict'
 const switcher = document.querySelector('.btn');
 
-const scroller = document.querySelector('.aboutmescrl');
+const scrollabout = document.querySelector('.aboutmescrl');
+
+const scrollcreate = document.querySelector('.creativeprojscrl')
+
+const scrolltop = document.querySelector('.topscrl')
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
@@ -17,8 +21,26 @@ switcher.addEventListener('click', function() {
     console.log('current theme name: ' + className)
 });
 
-scroller.addEventListener('click', function() {
-    let e = document.getElementById("el1");
+scrollabout.addEventListener('click', function() {
+    let e = document.getElementById("aboutme");
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    });
+});
+
+scrollcreate.addEventListener('click', function() {
+    let e = document.getElementById("creativeprojects");
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    });
+});
+
+scrolltop.addEventListener('click', function() {
+    let e = document.getElementById("banner");
     e.scrollIntoView({
         block: 'start',
         behavior: 'smooth',
