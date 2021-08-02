93400fc7a892a70839eef39c67b163859b6a4114
TheGiigacorn <brenlieb1@gmail.com>
2021-08-02 17:49:08 -0500

    Added more buttons and resumebox
    Added pdf viewer for resume as well as a hyperlink for resume. Added more navigation buttons and added scroll functionality to them. changed button color to something more attractive.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const scrollabout = document.querySelector('.aboutmescrl');
 
 const scrollcreate = document.querySelector('.creativeprojscrl');
 
+var scrollcode = document.querySelector('.codingprojscrl');
+
+var scrollresume = document.querySelector('.resumescrl');
+
 var topscroll = document.querySelectorAll('.topscrl');
 
 
@@ -20,6 +24,24 @@ scrollabout.addEventListener('click', function() {
 
 scrollcreate.addEventListener('click', function() {
     let e = document.getElementById("creativeprojects");
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    });
+});
+
+scrollcode.addEventListener('click', function() {
+    let e = document.getElementById("codingprojects");
+    e.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+    });
+});
+
+scrollresume.addEventListener('click', function() {
+    let e = document.getElementById("resumebox");
     e.scrollIntoView({
         block: 'start',
         behavior: 'smooth',
